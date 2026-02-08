dbaaaf175f8daeb1e9062ab50b9a19093c7c45af
RegionStat Developer <dev@regionstat.uz>
2026-02-08 12:16:58 +0500

    Fixed vite proxy URL from 3007 to 3000

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,12 +5,27 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     server: {
-        port: 3000,
+        port: 5173,
         proxy: {
             '/api': {
-                target: 'http://localhost:3007',
+                target: 'http://localhost:3000',
                 changeOrigin: true,
             },
         }
+    },
+    build: {
+        target: 'esnext',
+        minify: 'esbuild',
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    'vendor-react': ['react', 'react-dom', 'react-router-dom'],
+                    'vendor-antd': ['antd', '@ant-design/icons'],
+                    'vendor-utils': ['xlsx', 'dayjs', 'axios', 'i18next'],
+                    'vendor-charts': ['@ant-design/plots'],
+                }
+            }
+        },
+        chunkSizeWarningLimit: 1000
     }
 })
